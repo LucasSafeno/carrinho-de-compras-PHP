c23b8567fd208a1a42813271c5a002c84139636e
Lucas Safeno <lugnorio@gmail.com>
2024-10-11 23:45:23 +0000

    Carrinho de Compras com PHP#04 - Função de Remover e Limpar o carrinho de compras

--- app/classes/Cart.php
+++ app/classes/Cart.php
@@ -16,9 +16,17 @@ class Cart implements CartInterface
             $_SESSION['cart'][$product] += 1;
         
     }//  add
-    public function remove($product){}
+    public function remove($product){
+        if(!isset($_SESSION['cart'][$product])){
+            unset($_SESSION['cart'][$product]);
+        }
+    }// Remove
     public function quantity($product, $quantity){}
-    public function clear(){}
+    public function clear(){
+        if (isset($_SESSION['cart'])) {
+            unset($_SESSION['cart']);
+        }
+    }// clear
     public function cart(){
         if (isset($_SESSION['cart'])) {
             return $_SESSION['cart'];
--- public/index.php
+++ public/index.php
@@ -1,8 +1,22 @@
-<?php 
+<?php
+
+use app\classes\Cart;
+
+session_start();
+
+
 //Autoload
 require "../vendor/autoload.php";
 
 $products = require "../app/helpers/products.php";
+
+$cart = new Cart;
+
+//$cart->remove();
+//$cart->clear();
+$cart->dump();
+
+$productsInCart = $cart->cart()
 
 ?>
 
@@ -20,6 +34,7 @@ $products = require "../app/helpers/products.php";
 <body>
 
     <div id="container">
+        <h3>Cart : <?= count($productsInCart)?> | <a href="cart.php">Go to Cart</a> </h3>
     <ul>
         <?php foreach($products as $index => $product): ?> 
         <li> <?= $product['name'] ?> | R$ <?=number_format($product['price'],2,',','.')?></li><a href="add.php?id=<?=$index?>">add to cart</a>   
